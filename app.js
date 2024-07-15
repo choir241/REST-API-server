2fdc60ffb319da1603842dc7396231df8a94572d
choir27 <richardchoi54@gmail.com>
2024-07-15 16:21:51 -0400

    update app.js with appwrite database requests

--- app.js
+++ app.js
@@ -1,36 +1,84 @@
-const sdk = require('node-appwrite');
+const sdk = require("node-appwrite");
 
-const endpoint = 'https://cloud.appwrite.io/v1';
+const client = new sdk.Client();
+const endpoint = "https://cloud.appwrite.io/v1";
 
-async function seedUser() {
-    var testUser1 = {
-        email: 'yummyapple@email.com',
-        password: 'applesareyummy'
+client
+    .setEndpoint(endpoint)
+    .setProject(projectId)
+    .setKey(apiKey);
+
+    const databases = new sdk.Databases(client);
+
+    var todoDatabase;
+    var todoCollection;
+    
+async function prepareDatabase() {
+        todoDatabase = await databases.create(
+            sdk.ID.unique(),
+            'TodosDB'
+        );
+    
+        todoCollection = await databases.createCollection(
+            todoDatabase.$id,
+            sdk.ID.unique(),
+            'Todos'
+        );
+    
+        await databases.createStringAttribute(
+            todoDatabase.$id,
+            todoCollection.$id,
+            'title',
+            255,
+            true
+        );
+    
+        await databases.createStringAttribute(
+            todoDatabase.$id,
+            todoCollection.$id,
+            'description',
+            255, false,
+            'This is a test description'
+        );
+        await databases.createBooleanAttribute(
+            todoDatabase.$id,
+            todoCollection.$id,
+            'isComplete',
+            true
+        );
+}
+    
+async function seedDatabase() {
+    var testTodo1 = {
+        title: 'Buy apples',
+        description: 'At least 2KGs',
+        isComplete: true
     };
 
-    var testUser2 = {
-        email: 'washapple@email.com',
-        password: 'washapples'
+    var testTodo2 = {
+        title: 'Wash the apples',
+        isComplete: true
     };
 
-    var testUser3 = {
-        email: 'cutapples@email.com',
-        password: 'cutapples',
+    var testTodo3 = {
+        title: 'Cut the apples',
+        description: 'Don\'t forget to pack them in a box',
+        isComplete: false
     };
 
-    await createUser(
-        testUser1
+    await createDocument(
+        testTodo1
     );
-    await createUser(
-        testUser2
+    await createDocument(
+        testTodo2
     );
-    await createUser(
-        testUser3
+    await createDocument(
+        testTodo3
     );
 }
 
-async function createUser({email, password}){
-    await fetch(`${endpoint}/users`, {
+async function createDocument({title, description, isComplete}){
+    await fetch(`${endpoint}/databases/${databaseId}/collections/${collectionId}/documents`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -38,15 +86,18 @@ async function createUser({email, password}){
             'X-Appwrite-Key': apiKey,
         },
         body: JSON.stringify({
-            userId: sdk.ID.unique(),
-            email: email,
-            password: password
+            documentId: sdk.ID.unique(),
+            data:{
+                title,
+                description,
+                isComplete   
+            }
         })
     });
 }
 
-async function getUsers() {
-    const response = await fetch(`${endpoint}/users`, {
+async function getTodos() {
+    const response = await fetch(`${endpoint}/databases/${databaseId}/collections/${collectionId}/documents`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -55,15 +106,16 @@ async function getUsers() {
         }
     });
 
-    var data = await response.json();
-    
-    data.users.forEach(user => {
-        console.log(`User: ${user.email}\nCreated At: ${user.$createdAt}`);
+    var todos = await response.json();
+
+    todos.documents.forEach(todo => {
+        console.log(`Title: ${todo.title}\nDescription: ${todo.description}\nIs Todo Complete: ${todo.isComplete}\n\n`);
     });
 }
 
 async function runAllTasks() {
-    await seedUser();
-    await getUsers();
+    await prepareDatabase();
+    await seedDatabase();
+    await getTodos();
 }
 runAllTasks();
